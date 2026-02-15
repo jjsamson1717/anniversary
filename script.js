7ad9b2a810aacf23679ccd33eea534dfd69d56f9
jjsamson1717 <samsonjinkyjoy1718@gmail.com>
2026-02-15 22:10:40 +0800

    Update script.js

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ const gifStages = [
 
 const noMessages = [
     "No",
-    "Are you positive? 🤔",
-    "Pookie please... 🥺",
+    "Are you sure? 🤔",
+    "Pretty please... 🥺",
     "If you say no, I will be really sad...",
     "I will be very sad... 😢",
     "Please??? 💔",
